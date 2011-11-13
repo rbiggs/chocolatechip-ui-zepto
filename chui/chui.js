c35d57d79928189a2cc176f6c65805b5c51a7c36
Robert Biggs <rbiggs@ymail.com>
2011-11-13 13:51:39 -0800

    Minor update for deleted items

--- chui/chui.js
+++ chui/chui.js
@@ -572,7 +572,7 @@ $.fn.UISelectionList = function ( callback ) {
 			$(this).bind("click", function() {
 				$(this).siblings().removeClass("selected");
 				$(this).addClass("selected");
-				$(this).find("input").checked = true; 
+				$(this).find("input")[0].checked = true; 
 				if (callback) {
 					callback.call(callback, $(this).find("input"));
 				}
@@ -1538,5 +1538,154 @@ $(window).bind("orientationchange", function() {
 		if ($("rootview").length > 0) {
 			$("rootview").UIUnblock();
 		}
+	}
+});
+
+$.extend($, {
+	form2JSON : function(rootNode, delimiter) {
+		rootNode = typeof rootNode == 'string' ? $(rootNode)[0] : rootNode[0];
+		delimiter = delimiter || '.';
+		var formValues = getFormValues(rootNode);
+		var result = {};
+		var arrays = {};
+		
+		function getFormValues(rootNode) {
+			var result = [];
+			var currentNode = rootNode.firstChild;
+			while (currentNode) {
+				if (currentNode.nodeName.match(/INPUT|SELECT|TEXTAREA/i)) {
+					result.push({ name: currentNode.name, value: getFieldValue(currentNode)});
+				} else {
+					var subresult = getFormValues(currentNode);
+					result = result.concat(subresult);
+				}
+				currentNode = currentNode.nextSibling;
+			}
+			return result;
+		}
+		function getFieldValue(fieldNode) {
+			if (fieldNode.nodeName === 'INPUT') {
+				if (fieldNode.type.toLowerCase() === 'radio' || fieldNode.type.toLowerCase() === 'checkbox') {
+					if (fieldNode.checked) {
+						return fieldNode.value;
+					}
+				} else {
+					if (!fieldNode.type.toLowerCase().match(/button|reset|submit|image/i)) {
+						return fieldNode.value;
+					}
+				}
+			} else {
+				if (fieldNode.nodeName === 'TEXTAREA') {
+					return fieldNode.value;
+				} else {
+					if (fieldNode.nodeName === 'SELECT') {
+						return getSelectedOptionValue(fieldNode);
+					}
+				}
+			}
+			return '';
+		}
+		function getSelectedOptionValue(selectNode) {
+			var multiple = selectNode.multiple;
+			if (!multiple) {
+				return selectNode.value;
+			}
+			if (selectNode.selectedIndex > -1) {
+				var result = [];
+				$$("option", selectNode).forEach(function(item) {
+					if (item.selected) {
+						result.push(item.value);
+					}
+				});
+				return result;
+			}
+		}	  
+		formValues.forEach(function(item) {
+			var value = item.value;
+			if (value !== '') {
+				var name = item.name;
+				var nameParts = name.split(delimiter);
+				var currResult = result;
+				for (var j = 0; j < nameParts.length; j++) {
+					var namePart = nameParts[j];
+					var arrName;
+					if (namePart.indexOf('[]') > -1 && j == nameParts.length - 1) {
+						arrName = namePart.substr(0, namePart.indexOf('['));
+						if (!currResult[arrName]) currResult[arrName] = [];
+						currResult[arrName].push(value);
+					} else {
+						if (namePart.indexOf('[') > -1) {
+							arrName = namePart.substr(0, namePart.indexOf('['));
+							var arrIdx = namePart.replace(/^[a-z]+\[|\]$/gi, '');
+							if (!arrays[arrName]) {
+								arrays[arrName] = {};
+							}
+							if (!currResult[arrName]) {
+								currResult[arrName] = [];
+							}
+							if (j == nameParts.length - 1) {
+								currResult[arrName].push(value);
+							} else {
+								if (!arrays[arrName][arrIdx]) {
+									currResult[arrName].push({});
+									arrays[arrName][arrIdx] = 
+									currResult[arrName][currResult[arrName].length - 1];
+								}
+							}
+							currResult = arrays[arrName][arrIdx];
+						} else {
+							if (j < nameParts.length - 1) { 
+								if (!currResult[namePart]) {
+									currResult[namePart] = {};
+								}
+								currResult = currResult[namePart];
+							} else {
+								currResult[namePart] = value;
+							}
+						}
+					}
+				}
+			}
+		});
+		return result;
+	}		
+});
+$.extend($, {
+	templates : {},
+	 
+	template : function(str, data) {
+		if ($.templates[str]) {
+			str = $.templates[str];
+		} else {
+			str = str;
+		}
+		var tmpl = 'var p=[],print=function(){p.push.apply(p,arguments);};with(obj||{}){p.push(\''; 
+		var regex1; 
+		var regex2;
+		if (/\{\{/.test(str) || (/$\{/).test(str)) {
+			regex1 = /\$\{([\s\S]+?)\}/g;
+			regex2 = /\{\{([\s\S]+?)\}\}/g;
+		} else if (/\[\[/.test(str) || (/$\[/).test(str)) {
+			regex1 = /\$\[([\s\S]+?)\]/g;
+			regex2 = /\[\[([\s\S]+?)\]\]/g;
+		} else if (/<%=/.test(str) || (/<%/).test(str)) {
+			regex1 = /<%=([\s\S]+?)%>/g;
+			regex2 = /<%([\s\S]+?)%>/g;
+		}	
+		tmpl +=
+		  str.replace(/\\/g, '\\\\')
+			 .replace(/'/g, "\\'")
+			 .replace(regex1, function(match, code) {
+				return "'," + code.replace(/\\'/g, "'") + ",'";
+			 })
+			 .replace(regex2 || null, function(match, code) {
+				return "');" + code.replace(/\\'/g, "'")
+				.replace(/[\r\n\t]/g, ' ') + "p.push('";
+			 })
+			 .replace(/\r/g, '\\r')
+			 .replace(/\n/g, '\\n')
+			 .replace(/\t/g, '\\t') + "');} return p.join('');";
+		var fn = new Function('obj', tmpl);
+		return data ? fn(data) : fn;
 	}
 });
